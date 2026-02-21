a641de2036ec9947002aa463ccaceb6470db54f6
bisehshshrestha <bisheshshrestha652@gmail.com>
2026-02-21 15:42:31 +0545

    created Repo for All Reack Hooks

---
+++ hooks/useState/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
+      <button onClick={() => setCounter((prev) => prev - 1)}>Decrease</button>
+    </div>
+  );
+};
+
+export default Counter;
